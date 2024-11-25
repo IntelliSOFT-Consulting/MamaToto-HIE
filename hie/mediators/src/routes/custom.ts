50b8f52e45eeca005d5a3f890b88c893457f4a2d
Amolo <railamolo@gmail.com>
2024-11-25 14:58:03 +0000

    Checkpoint 25 Nov 2024

--- hie/mediators/src/routes/custom.ts
+++ hie/mediators/src/routes/custom.ts
@@ -43,7 +43,7 @@ router.post('/Patient', async (req, res) => {
         if (TEST_PHONE_NUMBERS.indexOf(`${data?.telecom?.[0]?.value ?? data?.telecom?.[1]?.value}`) > -1) {
             console.log("...redirecting")
             try {
-                let response = await redirectToDev("/fhir/Patient", data);
+                let response = await redirectToDev("/fhir/Patient", data, "POST");
                 console.log(JSON.stringify(response));
                 if (response.resourceType == "OperationOutcome") {
                     res.statusCode = 400;
@@ -129,7 +129,7 @@ router.post('/QuestionnaireResponse', async (req, res) => {
         if (patient.resourceType === "OperationOutcome"){
             console.log("...redirecting");
             try {
-                let response = await redirectToDev("/fhir/QuestionnaireResponse", data);
+                let response = await redirectToDev("/fhir/QuestionnaireResponse", data, "POST");
                 if (response.resourceType == "OperationOutcome") {
                     res.statusCode = 400;
                     res.json(response);
@@ -171,6 +171,151 @@ router.post('/QuestionnaireResponse', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.statusCode = 400;
+        res.json({
+            "resourceType": "OperationOutcome",
+            "id": "exception",
+            "issue": [{
+                "severity": "error",
+                "code": "exception",
+                "details": {
+                    "text": `${JSON.stringify(error)}`
+                }
+            }]
+        });
+        return;
+    }
+});
+
+router.post("/Observation", async (req, res) => {
+    try {
+      let data = req.body;
+      if (data.resourceType !== "Observation") {
+        res.statusCode = 400;
+        res.json({
+          resourceType: "OperationOutcome",
+          id: "exception",
+          issue: [
+            {
+              severity: "error",
+              code: "exception",
+              details: {
+                text: `Invalid Observation Resource`,
+              },
+            },
+          ],
+        });
+        return;
+      }
+  
+      let patient = await (
+        await FhirApi({ url: `/${data?.subject?.reference}` })
+      ).data;
+      // support [test phone number -> dev]
+      console.log(patient);
+      if (TEST_PHONE_NUMBERS.indexOf(`${patient?.telecom?.[0]?.value ?? patient?.telecom?.[1]?.value}`) > -1) {
+      if (patient.resourceType === "Observation") {
+        console.log("...redirecting");
+        try {
+          let response = await redirectToDev("/fhir/Observation", data);
+          if (response.resourceType == "OperationOutcome") {
+            res.statusCode = 400;
+            res.json(response);
+            return;
+          }
+          res.statusCode = 201;
+          res.json(response);
+          return;
+        } catch (error) {
+          res.statusCode = 400;
+          res.json({
+            resourceType: "OperationOutcome",
+            id: "exception",
+            issue: [
+              {
+                severity: "error",
+                code: "exception",
+                details: {
+                  text: `${JSON.stringify(error)}`,
+                },
+              },
+            ],
+          });
+          return;
+        }
+      }
+  
+      // default & production [save to SHR]
+      let shrResponse = await (
+        await FhirApi({
+          url: "/Observation",
+          method: "POST",
+          data: JSON.stringify(data),
+        })
+      ).data;
+      if (shrResponse.resourceType === "OperationOutcome") {
+        res.statusCode = 400;
+        res.json(shrResponse);
+        return;
+      }
+      res.statusCode = 200;
+      res.json(shrResponse);
+      return;
+    }
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 400;
+      res.json({
+        resourceType: "OperationOutcome",
+        id: "exception",
+        issue: [
+          {
+            severity: "error",
+            code: "exception",
+            details: {
+              text: `${JSON.stringify(error)}`,
+            },
+          },
+        ],
+      });
+      return;
+    }
+  });
+
+
+router.get('/Patient/:id', async (req, res) => {
+    try {
+        let {id} = req.params;
+
+        let patient = await (await FhirApi({ url: `/Patient/${id}` })).data;
+        // support [test phone number -> dev]
+        console.log(patient);
+        // if (TEST_PHONE_NUMBERS.indexOf(`${patient?.telecom?.[0]?.value ?? patient?.telecom?.[1]?.value}`) > -1) {
+        if (patient.resourceType === "OperationOutcome"){
+            console.log("...checking in dev");
+            try {
+                let response = await redirectToDev(`/fhir/Patient/${id}?_format=json`, null, "GET");
+                console.log(JSON.stringify(response));
+                if (response.resourceType == "OperationOutcome") {
+                    res.statusCode = 404;
+                    res.json(response);
+                    return;
+                }
+                res.statusCode = 200;
+                res.json(response);
+                return;
+            } catch (error) {
+                res.statusCode = 404;
+                res.json(patient);
+                return;
+            }
+
+        }
+        res.statusCode = 200;
+        res.json(patient);
+        return;
+    } catch (error) {
+        console.error(error);
+        res.statusCode = 404;
         res.json({
             "resourceType": "OperationOutcome",
             "id": "exception",
