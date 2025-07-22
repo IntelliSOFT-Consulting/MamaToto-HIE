170bbe7e812cb07541bc62de7e3cffcc651bf1f7
Brian Amolo <railamolo@gmail.com>
2025-07-22 16:30:05 +0300

    support multiple HCM facilities

--- hie/mediators/src/lib/consent.ts
+++ hie/mediators/src/lib/consent.ts
@@ -1,13 +1,13 @@
 import { FhirApi } from "./utils"
 
 const allowedFacilityExtension = (facilityId: string, facilityName: string) => {
-    return  {
+    return {
         "url": "http://example.org/fhir/StructureDefinition/facility-allowlist",
         "valueReference": {
-          "reference": `Organization/${facilityId}`,
-          "display": facilityName
+            "reference": `Organization/${facilityId}`,
+            "display": facilityName
         }
-      }
+    }
 }
 
 export const validateConsent = async (patientId: string, facilityId: string) => {
@@ -39,7 +39,8 @@ export const issueConsent = async (patientId: string, facilityId: string) => {
 
 
         if (consent.resourceType != "Consent") {
-            consent = (await FhirApi(`/Consent/${patientData?.id}`, {
+            consent = (await FhirApi({
+                url: `/Consent/${patientData?.id}`,
                 method: 'PUT',
                 data: {
                     resourceType: "Consent",
@@ -70,20 +71,19 @@ export const issueConsent = async (patientId: string, facilityId: string) => {
         // add consent extensions
         let consentedFacilities: Array<any> = consent?.provision?.extension ?? [];
 
-        let consentedFacilityIds = consentedFacilities.map((ext:any)=>{
+        let consentedFacilityIds = consentedFacilities.map((ext: any) => {
             return ext?.valueReference?.reference.split('/')[1];
         })
 
-        if (consentedFacilityIds.indexOf(facilityId) > -1){
+        if (consentedFacilityIds.indexOf(facilityId) > -1) {
             let consentResponse = { ...consent, provision: { type: "permit", extension: [allowedFacilityExtension(facilityId, facilityId)] } }
             return consentResponse;
         }
 
-        console.log(consentedFacilityIds);
 
         consentedFacilities.push(allowedFacilityExtension(facilityId, facilityId));
         consent.provision.extension = consentedFacilities;
-        const response = (await FhirApi(`/Consent/${patientData?.id}`, { method: 'PUT', data: consent }));
+        const response = (await FhirApi({ url: `/Consent/${patientData?.id}`, method: 'PUT', data: consent }));
         let consentResponse = { ...response.data, provision: { type: "permit", extension: [allowedFacilityExtension(facilityId, facilityId)] } }
 
         return consentResponse;
